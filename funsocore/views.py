3bc692f3897d7f876e63f4398c8c8e41befc937c
Jorge Andres Marles Florez <jamarlesf@gmail.com>
2022-11-16 00:24:00 -0500

    Actualizaccion Frontend

--- funsocore/views.py
+++ funsocore/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView
+from . import models
 # Create your views here.
+
+def post_lista(request):
+    posts = models.Post.objects.filter(state__exact=1)
+    #posts = models.Post.objects.all()
+    context = {'posts': posts}
+    return render(request, 'blogs/blog_list.html', context)
+
+def post_detalle(request, pk):
+    post = get_object_or_404(models.Post, pk=pk)
+    context = {'post': post}
+    return render(request, 'blogs/blog_detalle.html', context)
+
