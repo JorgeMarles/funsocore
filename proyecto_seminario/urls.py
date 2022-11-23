779894772a3ef687a99d13f855457f4db37cedd2
Jorge Andres Marles Florez <jamarlesf@gmail.com>
2022-11-22 21:14:31 -0500

    FUNSOCORE 0.2.1 frontend y acerca de actualizados

--- proyecto_seminario/urls.py
+++ proyecto_seminario/urls.py
@@ -13,36 +13,41 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.views.generic import TemplateView
+from django.contrib.auth import views
+from funsocore.forms import LoginUserPersonalizado
 from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
-admin.site.site_header = 'Funsocore'                    # default: "Django Administration"
-admin.site.index_title = 'Admin'                 # default: "Site administration"
-admin.site.site_title = 'Funsocore' # default: "Django site admin"
+# default: "Django Administration"
+admin.site.site_header = 'Funsocore'
+# default: "Site administration"
+admin.site.index_title = 'Admin'
+admin.site.site_title = 'Funsocore'  # default: "Django site admin"
 
-from funsocore.forms import LoginUserPersonalizado
-from django.contrib.auth import views
-from django.views.generic import TemplateView
 
 urlpatterns = [
-   
-    path('',views.TemplateView.as_view(template_name='ppal.html'),name='ppal'),
-    
-    path('donar',views.TemplateView.as_view(template_name='collab.html'),name='collab'),
-     path('admin/login/', views.LoginView.as_view(
-        template_name='admin/login.html',
-        authentication_form=LoginUserPersonalizado),
-        name='login'
-    ),
+
+    path('', views.TemplateView.as_view(template_name='ppal.html'), name='ppal'),
+
+    path('donar/', views.TemplateView.as_view(
+        template_name='collab.html'), name='collab'),
+    path('admin/login/', views.LoginView.as_view(
+         template_name='admin/login.html',
+         authentication_form=LoginUserPersonalizado),
+         name='login'
+         ),
     path('admin/', admin.site.urls),
 
-    path('blogs/', include('funsocore.urls')), 
+    path('acercade/', views.TemplateView.as_view(template_name='about.html'), name='acercade'),
+
+    path('blogs/', include('funsocore.urls')),
 
     path("__reload__/", include("django_browser_reload.urls")),
-    
-] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
